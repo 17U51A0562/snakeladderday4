42644dca3159cc2e53ee54929df96560b037e129
17U51A0562 <89656457+17U51A0562@users.noreply.github.com>
2021-10-06 22:21:52 +0530

    this is a roll dice problem

---
+++ rolldiceUC2.java
@@ -0,0 +1,14 @@
+/*
+*The Player rolls the die to get a number between 1 to 6. 
+*/
+
+public class rolldiceUC2 {
+	public static void main(String[] args) {
+
+		// to roll a dice
+		int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
+		System.out.println("diceNumber is : " + diceNumber);
+
+	}
+
+}
