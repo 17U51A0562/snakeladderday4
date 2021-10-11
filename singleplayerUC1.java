6e12130321206b5dcc0144836961fe7094cbed09
17U51A0562 <89656457+17U51A0562@users.noreply.github.com>
2021-10-11 11:00:43 +0530

    Update singleplayerUC1.java

--- singleplayerUC1.java
+++ singleplayerUC1.java
@@ -2,7 +2,7 @@
 *Snake and ladder game played with single player at start position 0
 *
 */
-public class singleplayerUC1 {
+public class SingleplayerUC1 {
 	public static void main(String[] args) {
 
 		// starting position of player
